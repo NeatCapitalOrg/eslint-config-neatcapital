eddd173f4adb65e3a15d5e088a3b20305c559d22
Jared <jmwbarnett@gmail.com>
2019-04-09 11:30:18 -0600

    Fixing one more warning

--- index.js
+++ index.js
@@ -7,10 +7,7 @@ module.exports = {
   },
   "extends": "eslint:recommended",
   "parserOptions": {
-    "ecmaVersion": 2017,
-    "ecmaFeatures": {
-      "experimentalObjectRestSpread": true
-    }
+    "ecmaVersion": 2017
   },
   "rules": {
     "array-bracket-spacing": "error",
